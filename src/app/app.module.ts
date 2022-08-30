bb4250e60015428ebe4aaa517e75b9ebaafcee13
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2022-08-30 13:25:15 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VehiclesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
